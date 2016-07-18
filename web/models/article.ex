779999662b90ef90bdffdb41a87173550becdd3a
tsub <tsubasatakayama511@gmail.com>
2016-07-18 13:29:03 +0900

    $ mix phoenix.gen.html Article articles title:string url:string og_title:string og_image:string og_description:text

---
+++ web/models/article.ex
@@ -0,0 +1,27 @@
+defmodule TokyoexHandsonDemo.Article do
+  use TokyoexHandsonDemo.Web, :model
+
+  schema "articles" do
+    field :title, :string
+    field :url, :string
+    field :og_title, :string
+    field :og_image, :string
+    field :og_description, :string
+
+    timestamps
+  end
+
+  @required_fields ~w(title url og_title og_image og_description)
+  @optional_fields ~w()
+
+  @doc """
+  Creates a changeset based on the `model` and `params`.
+
+  If no params are provided, an invalid changeset is returned
+  with no validation performed.
+  """
+  def changeset(model, params \\ :empty) do
+    model
+    |> cast(params, @required_fields, @optional_fields)
+  end
+end
