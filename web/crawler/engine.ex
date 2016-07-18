cdba62f520064f6d30c8335f7addb280e327b9e2
tsub <tsubasatakayama511@gmail.com>
2016-07-18 15:50:34 +0900

    Add Crawler modules

---
+++ web/crawler/engine.ex
@@ -0,0 +1,51 @@
+defmodule TokyoexHandsonDemo.Crawler.Engine do
+  require Logger
+
+  def get_site(url) do
+    Logger.debug "### Get #{url} ###"
+    %{body: body} = HTTPoison.get!(url)
+    body
+  end
+
+  def crawl(url, depth, pattern) do
+    crawl(url, url, depth, pattern)
+  end
+
+  defp crawl(url, _base_url, 0, _) do
+    Logger.debug "### Crawl #{url} ###"
+
+    if TokyoexHandsonDemo.Crawler.Storage.has_page?(url) do
+      Logger.debug "### already put #{url} ###"
+    else
+      Task.async(fn ->
+        site = get_site(url) |> TokyoexHandsonDemo.Crawler.Parser.parse_html
+        TokyoexHandsonDemo.Crawler.Storage.put_page(url, site)
+      end)
+    end
+  end
+
+  defp crawl(url, base_url, depth, pattern) do
+    Logger.debug "### Crawling #{url}, depth=#{inspect depth} ###"
+    body = get_site(url)
+    site = body |> TokyoexHandsonDemo.Crawler.Parser.parse_html
+
+    TokyoexHandsonDemo.Crawler.Storage.put_page(url, site)
+
+    TokyoexHandsonDemo.Crawler.Parser.parse_link(body, pattern)
+    |> Enum.map(fn url ->
+      url
+      |> normalize_url(base_url)
+      |> crawl(depth - 1, pattern)
+    end)
+    |> Enum.reject(&(&1 == :ok))
+    |> Enum.each(&Task.await(&1, 10_000))
+  end
+
+  defp normalize_url(url, base_url) do
+    if String.starts_with?(url, ".") or String.starts_with?(url, "/") do
+      Path.join(base_url, url)
+    else
+      url
+    end
+  end
+end
---
+++ web/crawler/storage.ex
@@ -0,0 +1,56 @@
+defmodule TokyoexHandsonDemo.Crawler.Storage do
+  require Logger
+
+  def start_link do
+    Agent.start_link(fn -> Map.new end, name: __MODULE__)
+  end
+
+  def has_page?(url) do
+    Agent.get(__MODULE__, fn map -> Map.has_key?(map, url) end)
+  end
+
+  def put_page(url, pages) do
+    Logger.debug "--- Put #{url} ---"
+    Agent.update(__MODULE__, fn map -> Map.put(map, url, pages) end)
+  end
+
+  def clear_all do
+    Logger.debug "--- Clear All ---"
+    Agent.update(__MODULE__, fn _map -> Map.new end)
+  end
+
+  def keys do
+    Logger.debug "--- Show keys ---"
+    Agent.get(__MODULE__, fn map -> Map.keys(map) end)
+  end
+
+  def dump do
+    Logger.debug "--- Dump All ---"
+    Agent.get(__MODULE__, fn map -> map end)
+  end
+
+  def store_to_db do
+    Logger.debug "--- Save storage to DB ---"
+    dump
+    |> Enum.each(fn {url, data} -> store_article_to_db(url, data) end)
+  end
+
+  def store_article_to_db(url, data) do
+    try do
+      if TokyoexHandsonDemo.Article.exist_with_url(url) do
+        Logger.debug "#{url} is already inserted!"
+      else
+        TokyoexHandsonDemo.Repo.insert!(%TokyoexHandsonDemo.Article{
+          url: url,
+          title: data |> Map.get(:title),
+          og_title: data |> Map.get(:og_title),
+          og_image: data |> Map.get(:og_image),
+          og_description: data |> Map.get(:og_description)
+        })
+        Logger.debug "#{url} is inserted!"
+      end
+    rescue
+      _ -> Logger.debug "#{url} is rescued!"
+    end
+  end
+end
