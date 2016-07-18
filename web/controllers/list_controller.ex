9b84f0baad8f253b1f03c8b126c938de232e6cd7
tsub <tsubasatakayama511@gmail.com>
2016-07-18 15:50:19 +0900

    Add Logger

--- web/controllers/list_controller.ex
+++ web/controllers/list_controller.ex
@@ -1,7 +1,11 @@
 defmodule TokyoexHandsonDemo.ListController do
   use TokyoexHandsonDemo.Web, :controller
 
+  require Logger
+
   def index(conn, _params) do
+    Logger.info("/list accessed!")
+
     list = TokyoexHandsonDemo.Repo.all(TokyoexHandsonDemo.Article)
     render conn, "index.html", list: list
   end
